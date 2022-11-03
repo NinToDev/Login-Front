212bf42762300eff736a75ce6907621ed70b2ef5
NintoDev <willintong9612@hotmail.com>
2022-11-03 09:24:06 -0500

    firtscommit, creation project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioSesionComponent } from './paginas/inicio-sesion/inicio-sesion.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSesionComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
